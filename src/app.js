460236c4598c2b24fd5f31e70c8372a211eca21c
unknown <shukumar542@gmail.com>
2022-09-04 19:25:11 +0600

    portfolio

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const threeBar = document.getElementById('menuBar');
+const menuOption = document.getElementById('menu');
+threeBar.addEventListener('click',()=>{
+      menuOption.classList.toggle('hidden')
+})
